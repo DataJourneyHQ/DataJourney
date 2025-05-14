4e7d72f04abccd13a75294102a8336364163cb9d
sayantikabanik <sayantikabanik122@gmail.com>
2025-05-14 14:24:42 +0700

    modified indexing, dataset and more

--- analytics_framework/rag_system/setup_chroma_vector_db.py
+++ analytics_framework/rag_system/setup_chroma_vector_db.py
@@ -1,16 +1,40 @@
 import chromadb
+import pandas as pd
+import numpy as np
+
+# what is happening here?
+# 1: read the dataset
+# 2: convert to string (that's the format the db expects)
+# 3: preferably set the index and then add to the db collection / or start simple
+YT_comments = pd.read_csv("../intake/data/YoutubeCommentsDataSet.csv", dtype=str, nrows=100)
+YT_comments.dropna(inplace=True)
+YT_comments.reset_index(inplace=True)
+print(YT_comments.dtypes)
+
+
+documents = YT_comments['Comment'].tolist()
+print(len(documents))
+metadata = []
+ids = []
+
+for idx in range(len(YT_comments)):
+    metadata.append({"Comment": YT_comments['Comment'][idx]})
+    ids.append(str(YT_comments['index'][idx]))
+
+# the good stuff
 chroma_client = chromadb.Client()
+collection = chroma_client.get_or_create_collection(name="YT_comments_new")
+collection.add(documents=documents, ids=ids)
 
-collection = chroma_client.get_or_create_collection(name="datajourney")
+# check the created collection
+collection.peek()
+collection.count()
 
-collection.upsert(
-    documents=["../intake/data/global_bleaching_environmental.csv"], #TODO: doesn't work, read and loop insertion
-    ids=["dj_01"]
-)
-
+# let's query this
 results = collection.query(
-    query_texts=["This is a query document about North Norman's Pond Cay Patch"],
-    n_results=2
+    query_texts=["apple pay and the physical credit card", "2009 macbook pro"],
+    n_results=3,
 )
-
+# voila mix multiple query types to arrive at the desired outcome
+print("the outcome")
 print(results)
