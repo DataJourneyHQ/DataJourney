84a9c0b1d9ed2b38588cb59ae97de869010e7962
sayantikabanik <sayantikabanik122@gmail.com>
2025-05-16 16:57:53 +0700

    linter updated

---
+++ analytics_framework/rag_system/rag_without_context_memory.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+import numpy as np
+from analytics_framework.langchain.hello_world_lc import get_answer
+from setup_chroma_vector_db import
+
+if __name__ == '__main__':
+    get_answer()
+
+
+# import the langchain hello world example
+# import YT dataset to setuo embedding
+# TODO: think monitoring
--- analytics_framework/rag_system/setup_chroma_vector_db.py
+++ analytics_framework/rag_system/setup_chroma_vector_db.py
@@ -6,35 +6,54 @@ import numpy as np
 # 1: read the dataset
 # 2: convert to string (that's the format the db expects)
 # 3: preferably set the index and then add to the db collection / or start simple
-YT_comments = pd.read_csv("../intake/data/YoutubeCommentsDataSet.csv", dtype=str, nrows=100)
-YT_comments.dropna(inplace=True)
-YT_comments.reset_index(inplace=True)
-print(YT_comments.dtypes)
+
+def YT_data_read_process(file_path, num_rows, col_to_process):
+    """
+    :param file_path:
+    :param num_rows:
+    :param col_to_process:
+    :return:
+    """
+    YT_comments = pd.read_csv("../intake/data/YoutubeCommentsDataSet.csv", dtype=str, nrows=100)
+    YT_comments.dropna(inplace=True)
+    YT_comments.reset_index(inplace=True)
+    documents = YT_comments['Comment'].tolist()
+    return documents, YT_comments
+    # print(YT_comments.dtypes)
 
 
-documents = YT_comments['Comment'].tolist()
-print(len(documents))
-metadata = []
-ids = []
+def chromdb_processing(documents, db_collection_name):
+    """
+    :param documents:
+    :return:
+    """
+    documents = YT_comments['Comment'].tolist()
+    print(len(documents))
+    metadata = []
+    ids = []
 
-for idx in range(len(YT_comments)):
-    metadata.append({"Comment": YT_comments['Comment'][idx]})
-    ids.append(str(YT_comments['index'][idx]))
+    for idx in range(len(YT_comments)):
+        metadata.append({"Comment": YT_comments['Comment'][idx]})
+        ids.append(str(YT_comments['index'][idx]))
 
 # the good stuff
-chroma_client = chromadb.Client()
-collection = chroma_client.get_or_create_collection(name="YT_comments_new")
-collection.add(documents=documents, ids=ids)
+    chroma_client = chromadb.Client()
+    collection = chroma_client.get_or_create_collection(name="YT_comments_new")
+    collection.add(documents=documents, ids=ids)
 
-# check the created collection
-collection.peek()
-collection.count()
+    # check the created collection
+    collection.peek()
+    collection.count()
 
-# let's query this
-results = collection.query(
-    query_texts=["2009 macbook pro"],
-    n_results=1,
-)
-# voila mix multiple query types to arrive at the desired outcome
-print("the outcome")
-print(results)
+    # let's query this
+    results = collection.query(
+        query_texts=["2009 macbook pro"],
+        n_results=1,
+    )
+    return results
+
+if __name__ == '__main__':
+
+    # voila mix multiple query types to arrive at the desired outcome
+    print("the outcome")
+    print(results)
