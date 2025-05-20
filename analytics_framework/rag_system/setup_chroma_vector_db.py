f672569a57afcb2f5c915aeda4669324d1a0c85b
sayantikabanik <sayantikabanik122@gmail.com>
2025-05-20 16:44:27 +0700

    modular structure implemented

--- analytics_framework/rag_system/setup_chroma_vector_db.py
+++ analytics_framework/rag_system/setup_chroma_vector_db.py
@@ -20,23 +20,26 @@ def yt_data_read_process(file_path, num_rows, col_to_process):
     yt_comments = pd.read_csv(file_path, dtype=str, nrows=num_rows)
     yt_comments.dropna(inplace=True)
     yt_comments.reset_index(inplace=True)
-    return yt_comments[col_to_process].tolist()
 
-
-def chromadb_processing(documents, db_collection_name, what_to_query, result_num=1):
-    """
-    :param documents: list,  pre-processed data
-    :param db_collection_name: str, nice name for the collection
-    :param what_to_query: str, drop your fav query text
-    :param result_num: int default set to 01
-    :return: str, query outcome based on closest vector
-    """
     metadata = []
     ids = []
 
-    for idx in range(len(YT_comments)):
-        metadata.append({"Comment": YT_comments['Comment'][idx]})
-        ids.append(str(YT_comments['index'][idx]))
+    for idx in range(len(yt_comments)):
+        metadata.append({"Comment": yt_comments['Comment'][idx]})
+        ids.append(str(yt_comments['index'][idx]))
+
+    return yt_comments[col_to_process].tolist(), ids
+
+
+def chromadb_processing(documents, ids, db_collection_name, what_to_query, result_num=1):
+    """
+    :param documents: list,  pre-processed data
+    :param ids: customised ids to generate embeddings
+    :param db_collection_name: str, nice name for the collection
+    :param what_to_query: str, drop your fav query text
+    :param result_num: int, default set to 01
+    :return: str, query outcome based on closest vector
+    """
 
     # the good stuff
     chroma_client = chromadb.Client()
@@ -56,16 +59,15 @@ def chromadb_processing(documents, db_collection_name, what_to_query, result_num
 
 
 if __name__ == '__main__':
-    documents = yt_data_read_process("../intake/data/YoutubeCommentsDataSet.csv",
-                                     100,
-                                     "Comment"
-                                     )
+    documents, ids = yt_data_read_process("../intake/data/YoutubeCommentsDataSet.csv",
+                                          100,
+                                          "Comment"
+                                          )
     results = chromadb_processing(documents,
+                                  ids,
                                   "YT_comments_new",
-                                  "My macbook broke",
-                                  result_num=2
+                                  "My macbook broke in an accident",
                                   )
 
     # voila mix multiple query types to arrive at the desired outcome
-    print("the outcome")
-    print(results)
+    print(f"The outcome for the query text \n {results}")
