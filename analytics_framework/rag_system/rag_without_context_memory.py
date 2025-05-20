beeb1ffe76214f81fa16420693fdff96db4f3aa9
sayantikabanik <sayantikabanik122@gmail.com>
2025-05-20 16:46:07 +0700

    basic setup updated

--- analytics_framework/rag_system/rag_without_context_memory.py
+++ analytics_framework/rag_system/rag_without_context_memory.py
@@ -2,12 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from analytics_framework.langchain.hello_world_lc import get_answer
-from setup_chroma_vector_db import
+from setup_chroma_vector_db import yt_data_read_process, chromadb_processing
 
 if __name__ == '__main__':
     get_answer()
-
-
-# import the langchain hello world example
-# import YT dataset to setuo embedding
-# TODO: think monitoring
