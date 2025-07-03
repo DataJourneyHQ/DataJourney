b31bb8ec30033dc15bd6dc63ba7741cd2413c83a
sayantikabanik <sayantikabanik122@gmail.com>
2025-07-03 12:47:37 +0700

    refresh of setup file with additonal details

--- setup.py
+++ setup.py
@@ -5,11 +5,18 @@ Package manager for analytics_framework
 from setuptools import find_packages, setup
 
 setup(
+    name="analytics_framework",
+    version="1.0.0",
     author="sayantikabanik",
     description="Design first open source data management toolkit",
-    include_package_data=True,
-    name="analytics_framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DataJourneyHQ/DataJourney",
     packages=find_packages(),
-    url="https://github.com/sayantikabnik/DataJourney",
-    version="1.0.0"
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    license="Apache-2.0"
 )
